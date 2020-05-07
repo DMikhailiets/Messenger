4b7ccd0223010a5442c02b69c999080c16380c6f
DMikhailiets <dmikhailiets21@gmail.com>
2020-05-07 08:59:19 +0500

    added backend functional

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,34 @@
-const func = (a: number, b: number ) => {
-    return a + b
-}
+import mongoose from 'mongoose'
+import express from 'express'
+import bodyParser from 'body-parser'
 
-console.log(func(1,2))
+import  { UserController } from './controllers'
+
+mongoose.connect('mongodb://localhost:27017/chat', {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: true
+});
+
+const app = express();
+
+const User = new UserController();
+
+// app.use(bodyParser.urlencoded({ extended: false })) for multipart-form data
+app.use(bodyParser.json())
+
+app.get('/user/:id', User.show)
+app.delete('/user/:id', User.delete)
+app.post('/user/registration', User.create)
+
+
+
+
+// app.post('/create', function (req: express.Request, res: express.Response) {
+  
+// });
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+});
---
+++ backend/src/schemas/User.ts
@@ -0,0 +1,43 @@
+import mongoose, { Schema, Document } from 'mongoose'
+import validator from 'validator'
+
+export interface IUser extends Document{
+    email: string,
+    fullname: string,
+    password: string,
+    confirmed: boolean,
+    avatar: string,
+    confirm_hash: string,
+    last_seen: Date 
+}
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        validate: [validator.isEmail, 'Invalid email'],
+        unique: true
+    }, 
+    avatar: String,
+    confirm_hash: String,
+    fullname: {
+        type: String,
+        required: 'fullname is required' 
+    },
+    password: {
+        type: String,
+        required: 'password is required' 
+    },
+    confirmed: {
+        type: Boolean,
+        default: false
+    },
+    lastSeen: Date,
+    last_seen: Date,
+
+}, {
+    timestamps: true
+})
+
+const UserModel = mongoose.model<IUser>('User', UserSchema);
+
+export default UserModel
